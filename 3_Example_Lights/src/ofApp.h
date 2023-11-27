18ecb1cb6a6f9f162a1da31407d50e0574320220
moebiusSurfing <moebiussurfing@gmail.com>
2023-11-27 00:09:56 -0300

    '

--- 3_Example_Lights/src/ofApp.h
+++ 3_Example_Lights/src/ofApp.h
@@ -1,18 +1,20 @@
 /*
 
 	This example do not includes the main bigger classes from the addon:
-	ofxSurfingPBR.h neither the SurfingSceneManager.h.
+	`ofxSurfingPBR.h`` neither the `SurfingSceneManager.h``.
 	These both classes have multiple modules or features included by default,
-	(as it could be the CubeMap, the background object, the floor plane...etc)
+	(as it could be the CubeMap, the background object, the floor plane...etc).
+
 	Here we want to learn and create a standalone material 
-	and a standalone lights combo.
-	The material is applied to the floor plane and 
-	to the testing three prims scene that we will draw.
+	and a standalone kit of lights.
+	The material is applied to the floor plane 
+	and to the testing three prims scene that we will draw.
+
 	The lights bundled are 4 in total. One of each of the available types 
 	into OF core:
-	Point, Direct, Spot and Area. All with his settings on a gui 
-	to tweak all the internals.
-	All of them with all the settings persistent and ready 
+	`Point`, `Direct`, `Spot` and `Area``. 
+	All with his settings on a gui to tweak all the internals.
+	All of them with all the settings persistent, gui exposed and ready 
 	to use on any project.
 
 */
