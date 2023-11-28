478ffe877bdbba006436a44ed3bd8f922d7edeb3
moebiusSurfing <moebiussurfing@gmail.com>
2023-11-28 02:16:36 -0300

    Update ofApp.h

--- 6_Example_3D_Parts/src/ofApp.h
+++ 6_Example_3D_Parts/src/ofApp.h
@@ -10,7 +10,7 @@
 	to customize the scene.  
 	This class auto-creates a combo of the four default lights,  
 	to perform the light illumination,  
-	and allows queueing materials and colors to our scene.  
+	and allows queuing materials and colors to our scene.  
 	We will create a material for each part,  
 	so we can tweak each PBR material color and the PBR settings  
 	for each part independently.  
